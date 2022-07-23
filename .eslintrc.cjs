e6a2e71dff0d080cb639cd99d17c23473e15ab8f
Ian Macalinao <ian@aptos.is>
2022-07-23 11:32:00 -0500

    clean up linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,8 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 /** @type import('@typescript-eslint/utils').TSESLint.Linter.Config */
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
   extends: ["@saberhq/eslint-config"],
   parserOptions: {
-    project: ["tsconfig.json", "./**/tsconfig*.json"],
+    project: ["tsconfig.json", "./packages/*/tsconfig.json"],
   },
 };
